728fb6d3db8548812faef0aec8076263379aab27
felipe <felipepassada@gmail.com>
2023-11-07 15:32:59 -0300

    feat(mongo): add mongoose lib to setup connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express ,{ Application, } from 'express';
+import mongoose from 'mongoose';
 
 import dotenv from 'dotenv';
 import routes from './routes/';
@@ -10,5 +11,7 @@ const port:any = process.env.PORT || 8080;
 
 app.use(routes);
 app.listen(port);
+
+// mongoose.connect('mogodb://127.0.0.1:27017/test');
 
 console.log(`Server running at http://localhost:${port}`);
