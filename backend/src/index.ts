c63183436737fbee4055b8d70d8c6f3a6c463a8b
felipe <felipepassada@gmail.com>
2023-11-09 22:21:36 -0300

    feat(cors): add cors to handle allowed hosts
    configured to allow requests from the frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,17 @@
 import express ,{ Application, } from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 import dotenv from 'dotenv';
 import routes from './routes/';
+import { corsOptions } from '../utils/cors';
 
 dotenv.config()
 const app:Application = express();
 const port:any = process.env.PORT || 8080;
 
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(routes);
 app.listen(port);
---
+++ backend/utils/cors.ts
@@ -0,0 +1,9 @@
+const allowedServers = [
+  'http://localhost',
+  'http://localhost:4200',
+  'http://127.0.0.1'
+]
+
+export const corsOptions = {
+  origin: allowedServers
+}
