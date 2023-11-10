4b80097400074a8bba30ddcda1612c98289737ae
felipe <felipepassada@gmail.com>
2023-11-10 10:47:54 -0300

    refactor(env): change hardcoded url to dinamic env

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap, Observable } from 'rxjs';
+import { API_BASE_URL } from '../config/app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,11 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   fetchUsers(): Observable<any> {
-    //TODO: set all api variables to a const file
-    return this.httpClient.get('http://127.0.0.1:8080/users');
+    return this.httpClient.get(`${API_BASE_URL}/users`);
   }
 
   fetchDetails(id: string): Observable<any> {
-    return this.httpClient.get(`http://127.0.0.1:8080/users/${id}`)
+    return this.httpClient.get(`${API_BASE_URL}/users/${id}`)
   }
 
   // batchLoadUsers(): Observable<any> {
@@ -23,9 +23,10 @@ export class UserService {
   // }
 
   editUser(id: string, user: any): Observable<any> {
-    return this.httpClient.put(`http://127.0.0.1:8080/users/${id}`, user);
+    return this.httpClient.put(`${API_BASE_URL}/users/${id}`, user);
   }
+
   deleteUser(id: string): Observable<any> {
-    return this.httpClient.delete(`http://127.0.0.1:8080/users/${id}`);
+    return this.httpClient.delete(`${API_BASE_URL}/users/${id}`);
   }
 }
